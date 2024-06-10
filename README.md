c6f7b74fcf45e69430f66cb1d536729c79c41e1b
Huw McNamara <huwmcnamara@msn.com>
2024-06-10 09:12:17 +0100

    tweaks

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # little-timmy
 
 Little Timmy will try their best to find those unused Ansible variables.
+
+```sh
+cd repo/ansible/plays
+ansible-galaxy collection install -f -r requirements.yml -p .
+ansible-galaxy role install -f -r requirements.yml -p galaxy_roles
+
+pip3 install little-timmy
+little_timmy
+```
--- little_timmy/__main__.py
+++ little_timmy/__main__.py
@@ -13,6 +13,7 @@ from ansible.plugins.loader import init_plugin_loader
 from ansible.plugins.test.core import TestModule as CoreTests
 from glob import iglob
 from jinja2 import Environment, meta
+
 import argparse
 import os
 
@@ -26,6 +27,7 @@ MAGIC_VAR_NAMES = {
     "ansible_diff_mode",
     "ansible_facts",
     "ansible_forks",
+    "ansible_host",
     "ansible_host",
     "ansible_index_var",
     "ansible_inventory_sources",
@@ -54,6 +56,8 @@ MAGIC_VAR_NAMES = {
     "ansible_version",
     "group_names",
     "groups",
+    "groups",
+    "hostvars"
     "hostvars",
     "inventory_dir",
     "inventory_file",
@@ -66,13 +70,12 @@ MAGIC_VAR_NAMES = {
     "role_names",
     "role_path",
     "undef"
-    "undefined"
+    "undefined",
 }
 
 YAML_FILE_EXTENSION_GLOB = "*y*ml"
-
-EXTERNAL_DIRs = ["galaxy_roles", "ansible_collections"]
-DIRS_TO_EXCLUDE = ["molecule", "venv", "tests"]
+EXTERNAL_DEP_DIRS = ["galaxy_roles", "ansible_collections"]
+GLOBAL_DIRS_TO_EXCLUDE = ["molecule", "venv", "tests"]
 
 # there must be a better way to do this
 JINJA_ENV = Environment()
@@ -90,16 +93,23 @@ for fqdn, modules in filter_modules.items():
             JINJA_ENV.filters[name] = filter_func
 
 
-def get_files_in_folder(root_dir: str, folder: str, file_glob: str = "*", include_ext=False):
+def get_files_in_folder(root_dir: str, folder: str, file_glob: str = "*", include_ext=False, dirs_to_exclude: list[str] = []):
+    if not include_ext:
+        dirs_to_exclude = dirs_to_exclude + EXTERNAL_DEP_DIRS
+    dirs_to_exclude = dirs_to_exclude + GLOBAL_DIRS_TO_EXCLUDE
+
+    def should_exclude(path: str):
+        relative_path = os.path.dirname(os.path.relpath(path, root_dir))
+        return any(excluded_dir in relative_path for excluded_dir in dirs_to_exclude)
+
     return (
-        f for f in iglob(f"{root_dir}/**/{folder}/**/{file_glob}", recursive=True)
+        f for f in iglob(f"{root_dir}/{folder}/**/{file_glob}", recursive=True)
         if os.path.isfile(f)
-        and (include_ext or not any(excluded_dir in f for excluded_dir in EXTERNAL_DIRs))
-        and not any(excluded_dir in f for excluded_dir in DIRS_TO_EXCLUDE)
+        and not should_exclude(f)
     )
 
 
-def parse_jinja(value: str, all_referenced_vars: dict[str, set[str]], source: str):
+def parse_jinja(value: any, all_referenced_vars: dict[str, set[str]], source: str):
     parsed = JINJA_ENV.parse(value)
     referenced_vars = meta.find_undeclared_variables(parsed)
     for referenced_var in referenced_vars:
@@ -108,13 +118,16 @@ def parse_jinja(value: str, all_referenced_vars: dict[str, set[str]], source: st
         all_referenced_vars[referenced_var] = existing
 
 
-def parse_variable(var_name: str, var_value: str, all_declared_vars: dict[str, set[str]], all_referenced_vars: dict[str, set[str]], source: str):
-    # print(f"var {var_name}")
+def parse_variable(var_name: str, var_value: any, all_declared_vars: dict[str, set[str]], all_referenced_vars: dict[str, set[str]], source: str, root_dir: str):
     if var_name in MAGIC_VAR_NAMES:
         return
-    existing = all_declared_vars.get(var_name, set())
-    existing.add(source)
-    all_declared_vars[var_name] = existing
+    relative_path = os.path.dirname(os.path.relpath(source, root_dir))
+    external = any(
+        excluded_dir in relative_path for excluded_dir in EXTERNAL_DEP_DIRS)
+    if not external:
+        existing = all_declared_vars.get(var_name, set())
+        existing.add(source)
+        all_declared_vars[var_name] = existing
     parse_jinja(var_value, all_referenced_vars, source)
 
 
@@ -152,63 +165,62 @@ def main():
     # Process all the things
 
     # group_vars
-    for path in get_files_in_folder(directory, "group_vars", YAML_FILE_EXTENSION_GLOB):
+    for path in get_files_in_folder(directory, "**/group_vars", YAML_FILE_EXTENSION_GLOB):
         print(f"group_var {path}")
 
         contents = loader.load_from_file(path) or {}
         for var_name, var_value in contents.items():
             parse_variable(var_name, var_value,
-                           all_declared_vars, all_referenced_vars, path)
+                           all_declared_vars, all_referenced_vars, path, directory)
 
     # host_vars
-    for path in get_files_in_folder(directory, "host_vars", YAML_FILE_EXTENSION_GLOB):
+    for path in get_files_in_folder(directory, "**/host_vars", YAML_FILE_EXTENSION_GLOB):
         print(f"host_var {path}")
         contents = loader.load_from_file(path) or {}
         for var_name, var_value in contents.items():
             parse_variable(var_name, var_value,
-                           all_declared_vars, all_referenced_vars, path)
+                           all_declared_vars, all_referenced_vars, path, directory)
     # vars
-    for path in get_files_in_folder(directory, "vars", YAML_FILE_EXTENSION_GLOB, include_ext=True):
+    for path in get_files_in_folder(directory, "**/vars", YAML_FILE_EXTENSION_GLOB, include_ext=True):
         print(f"var file {path}")
         contents = loader.load_from_file(path) or {}
         for var_name, var_value in contents.items():
             parse_variable(var_name, var_value,
-                           all_declared_vars, all_referenced_vars, path)
+                           all_declared_vars, all_referenced_vars, path, directory)
 
     # defaults
-    for path in get_files_in_folder(directory, "defaults", YAML_FILE_EXTENSION_GLOB, include_ext=True):
+    for path in get_files_in_folder(directory, "**/defaults", YAML_FILE_EXTENSION_GLOB, include_ext=True):
         print(f"default {path}")
         contents = loader.load_from_file(path) or {}
         for var_name, var_value in contents.items():
             parse_variable(var_name, var_value,
-                           all_declared_vars, all_referenced_vars, path)
+                           all_declared_vars, all_referenced_vars, path, directory)
 
     # inventory
-    for path in get_files_in_folder(directory, "inventory"):
-        print(f"inv file {path}")
-        inventory = InventoryManager(loader=loader, sources=path)
-
-        # groups
-        for _, group_value in inventory.groups.items():
-            # vars in group
-            for var_name, var_value in group_value.vars.items():
-                parse_variable(var_name, var_value,
-                               all_declared_vars, all_referenced_vars, path)
-            # hosts in group
-            for host in group_value.hosts:
-                # vars in host
-                for var_name, var_value in host.vars.items():
+    for inv_folder in ["inventory", "inventories"]:
+        for path in get_files_in_folder(directory, inv_folder, dirs_to_exclude=["group_vars", "host_vars"]):
+            print(f"inv file {path}")
+            inventory = InventoryManager(loader=loader, sources=path)
+            # groups
+            for _, group_value in inventory.groups.items():
+                # vars in group
+                for var_name, var_value in group_value.vars.items():
                     parse_variable(var_name, var_value,
-                                   all_declared_vars, all_referenced_vars, path)
+                                   all_declared_vars, all_referenced_vars, path, directory)
+                # hosts in group
+                for host in group_value.hosts:
+                    # vars in host
+                    for var_name, var_value in host.vars.items():
+                        parse_variable(var_name, var_value,
+                                       all_declared_vars, all_referenced_vars, path, directory)
 
     # templates
-    for path in get_files_in_folder(directory, "templates", include_ext=True):
+    for path in get_files_in_folder(directory, "**/templates", include_ext=True):
         print(f"template file {path}")
         with open(path, "r") as f:
             parse_jinja(f.read(), all_referenced_vars, path)
 
     # playbook in current directory
-
     for path in iglob(f"{directory}/playbook*{YAML_FILE_EXTENSION_GLOB}", recursive=False):
         print(f"playbook {path}")
         with open(path, "r") as f:
@@ -216,14 +228,14 @@ def main():
                 f.read(), all_declared_vars, all_referenced_vars, path)
 
     # tasks files
-    for path in get_files_in_folder(directory, "tasks", YAML_FILE_EXTENSION_GLOB, True):
+    for path in get_files_in_folder(directory, "**/tasks", YAML_FILE_EXTENSION_GLOB, True):
         print(f"task file {path}")
         with open(path, "r") as f:
             check_raw_file_for_variables(
                 f.read(), all_declared_vars, all_referenced_vars, path)
 
     # handlers files
-    for path in get_files_in_folder(directory, "handlers", YAML_FILE_EXTENSION_GLOB, True):
+    for path in get_files_in_folder(directory, "**/handlers", YAML_FILE_EXTENSION_GLOB, True):
         print(f"handler file {path}")
         with open(path, "r") as f:
             check_raw_file_for_variables(
@@ -234,7 +246,7 @@ def main():
         all_declared_vars.pop(var_name, None)
 
     print("\n **unused vars** \n")
-
+    # filter out vars only declared in galaxy_roles and collections
     for var_name, var_locations in all_declared_vars.items():
         print(f"{var_name} at {var_locations}\n")
 
