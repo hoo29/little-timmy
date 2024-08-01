abad6d9b6940b2debb1e68aaac788149f25dbf00
Huw McNamara <huwmcnamara@msn.com>
2024-08-01 17:03:17 +0100

    add logging dave mode

--- little_timmy/__main__.py
+++ little_timmy/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import sys
 
 from .config_loader import find_and_load_config
 from .var_finder import find_unused_vars
@@ -16,13 +17,21 @@ def main():
                         help="set the logging level (default: INFO)")
     parser.add_argument(
         "-c", "--config-file", type=str, help="Config file to use. By default it will search all dirs to `/` for .little-timmy")
+    parser.add_argument("-d", "--dave-mode", default=False, type=bool,
+                        help="Make logging work on dave's macbook")
     args = parser.parse_args()
 
     log_level = getattr(logging, args.log_level.upper(), None)
     if not isinstance(log_level, int):
         raise ValueError(f"Invalid log level: {args.log_level}")
-    logging.basicConfig(level=log_level, format="%(message)s")
 
+    if args.dave_mode:
+        stderr_log_handler = logging.StreamHandler(stream=sys.stderr)
+        stderr_log_handler.setLevel(log_level)
+        stderr_log_handler.setFormatter(logging.Formatter("%(message)s"))
+        LOGGER.addHandler(stderr_log_handler)
+
+    logging.basicConfig(level=log_level, format="%(message)s")
     LOGGER.debug("starting")
     if args.directory == ".":
         directory = os.getcwd()
