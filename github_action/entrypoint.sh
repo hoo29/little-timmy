1fb7ba54f78313cb7f1916440feceafd044b80d1
Huw McNamara <huwmcnamara@msn.com>
2024-09-28 10:24:50 +0100

    Add github action

---
+++ github_action/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -le
+
+echo "Changing directory to $INPUT_DIRECTORY"
+cd "$INPUT_DIRECTORY"
+
+if [ -n "$INPUT_GALAXY_ROLE_REQUIREMENTS_FILE" ]; then
+    echo "Installing ansible galaxy roles from $INPUT_GALAXY_ROLE_REQUIREMENTS_FILE"
+    ansible-galaxy role install -f -r "$INPUT_GALAXY_ROLE_REQUIREMENTS_FILE" -p galaxy_roles
+fi
+
+if [ -n "$INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE" ]; then
+    echo "Installing ansible galaxy collections from $INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE"
+    ansible-galaxy collection install -f -r "$INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE" -p .
+fi
+
+if [ -n "$INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE" ]; then
+    echo "Installing ansible galaxy collections from $INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE"
+    ansible-galaxy collection install -f -r "$INPUT_GALAXY_COLLECTION_REQUIREMENTS_FILE" -p .
+fi
+
+printenv INPUT_ANSIBLE_VAULT >ansible_vault_password
+export ANSIBLE_VAULT_PASSWORD_FILE="ansible_vault_password"
+
+echo "Running little-timmy"
+# shellcheck disable=SC2086
+little-timmy $INPUT_ADDITIONAL_CLI_ARGS
+
+echo "Done"
